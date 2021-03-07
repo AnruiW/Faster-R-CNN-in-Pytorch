7dc11ad35edeb4aa9fc3ae6713bfaeac19b9662f
Anrui Wang <79292963+AnruiW@users.noreply.github.com>
2021-03-07 23:36:38 +0800

    Update rpn.py

--- net/rpn.py
+++ net/rpn.py
@@ -7,58 +7,72 @@ import cv2
 import numpy as np
 
 
-def generate_region_proposal(location_list, score_list):
-    print(location_list.shape)
-    print(score_list.shape)
+def compose_layers(net_config, in_channels, use_bn):
+    layers = []
+    for layer in net_config:
+        if layer == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels=in_channels, out_channels=layer, kernel_size=3, stride=1, padding=1)
+            if use_bn:
+                layers += [conv2d, nn.BatchNorm2d(layer), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = layer
+    return nn.Sequential(*layers)
 
 
+# the Conv part of VGG net
+def vgg_feature(net_type='D', in_channels=3, use_bn=False):
+    ConvNet_Configuration = {
+        'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+        'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+        'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+        'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+    }
 
-class rpn_net(nn.Module):
-    def __init__(self, feature_layer, anchor_num=9):
-        """
-        :param anchor_num: the number of anchor generate at each location
+    return compose_layers(ConvNet_Configuration[net_type], in_channels, use_bn)
 
-        Every anchor will predict 2 scores estimating the probability of object or not object.
-        Every anchor will predict 4 number corresponding to its location.
-        """
-        super(rpn_net, self).__init__()
 
-        self.feature = feature_layer
-        self.conv = nn.Sequential(
-            nn.Conv2d(512, 512, 3, 1, 1),
-            nn.ReLU()
+# the complete version of VGG net
+class vgg_net(nn.Module):
+    def __init__(self):
+        super(vgg_net, self).__init__()
+
+        self.features = vgg_feature()
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(
+            nn.Linear(512*7*7, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 1000)
         )
-        self.l_conv = nn.Conv2d(512, anchor_num * 4, 1, 1, 0)
-        self.s_conv = nn.Conv2d(512, anchor_num * 2, 1, 1, 0)
+        self._initialize_weights()
 
     def forward(self, x):
-        feature = self.feature(x)
-        rpn_feature = self.conv(feature)
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
 
-        location_list = self.l_conv(rpn_feature)
-        # the output of s_conv is (batch_size, 36, 7, 7)
-        # need to be converted into ()
-        location_list = location_list.permute(0, 2, 3, 1).contiguous().view(x.shape[0], -1, 4)
+    def get_feature_layer(self):
+        return self.features
 
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
 
-        score_list = self.s_conv(rpn_feature)
-        # the output of s_conv is (batch_size, 18, 7, 7)
-        # need to be converted into (batch_size, num of box, 2)
-        score_list = score_list.permute(0, 2, 3, 1).contiguous().view(x.shape[0], -1, 2)
-
-        return feature, location_list, score_list
-
-
-if __name__ == '__main__':
-    image = cv2.imread("test_image.jpg")
-    image = cv2.resize(image, dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
-    print(image.shape)
-    image = image.reshape((3, 224, 224))
-    image = torch.from_numpy(image[np.newaxis, :], )
-    image = image.float()
-    print(image.shape)
-    vgg = vgg_net()
-    rpn = rpn_net(vgg.get_feature_layer())
-    location_list, score_list = rpn(image)
-    generate_region_proposal(location_list, score_list)
 
